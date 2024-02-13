2ce83f3cf5e44b62a03a0164e4222f0b114167aa
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-13 13:33:35 +0200

    Fixed code.

--- src/App.js
+++ src/App.js
@@ -1,86 +1,105 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Steps />
       <Counter />
-      <Calendar />
-    </div>
-  );
-}
-function Steps() {
-  const [step, setStep] = useState(0);
-
-  function minusStep() {
-    if (step > 0) setStep(step - 1);
-  }
-
-  function addStep() {
-    if (step < 31) setStep(step + 1);
-  }
-
-  return (
-    <div>
-      <div className="buttons">
-        <button onClick={() => minusStep()}>-</button>
-        Step: {step}
-        <button
-          onClick={() => {
-            addStep();
-          }}
-        >
-          +
-        </button>
-      </div>
     </div>
   );
 }
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
-  function minusCount() {
-    if (count > 0) setCount(count - 1);
-  }
-
-  function addCount() {
-    if (count < 31) setCount(count + 1);
-  }
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   return (
     <div>
       <div className="buttons">
-        <button onClick={() => minusCount()}>-</button>
-        Count: {count}
-        <button
-          onClick={() => {
-            addCount();
-          }}
-        >
-          +
-        </button>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        Step: {step}
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <div className="buttons">
+          <button onClick={() => setCount((c) => c - step)}>-</button>
+          Count: {count}
+          <button onClick={() => setCount((c) => c + step)}>+</button>
+        </div>
+        <p>
+          {/**Not recommended to use nested ternary operators */}
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
       </div>
     </div>
   );
 }
 
-function Calendar() {
-  const [date, setDate] = useState(new Date());
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
+// export default function App() {
+//   // const [date, setDate] = useState(HDate);
+//   return (
+//     <div className="App">
+//       <Steps />
+//       <Counter />
+//       <HDate />
+//     </div>
+//   );
+// }
+// function Steps() {
+//   const [step, setStep] = useState(0);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+//   function minStep() {
+//     if (step > 0) setStep(step - 1);
+//   }
 
-  return (
-    <div>
-      <p> days from today is {date.toDateString()}</p>
-    </div>
-  );
-}
+//   function addStep() {
+//     if (step < 31) setStep(step + 1);
+//   }
+
+//   return (
+//     <div>
+//       <div className="buttons">
+//         <button onClick={minStep}>-</button>
+//         Step: {step.props.current.getFullYear()}
+//         <button onClick={addStep}>+</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function deCount() {
+//     if (count > 0) setCount(count - 1);
+//   }
+//   function inCount() {
+//     if (count < 31) setCount(count + 1);
+//   }
+
+//   return (
+//     <div>
+//       <div className="buttons">
+//         <button onClick={deCount}>-</button>
+//         Count: {count.props.current.getDate()}
+//         <button onClick={inCount}>+</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function HDate(props) {
+//   const current = new Date();
+//   const date = `${props.current.getDate()}-${
+//     current.getMonth() + 1
+//   }-${props.current.getFullYear()}`;
+//   return <p>"" days from today is {date}</p>;
+// }
